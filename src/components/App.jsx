36d90e5e89790cbcac0783337855fb30af5c840a
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-02-09 11:42:00 +0200

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,28 @@
+import user from 'components/SocialProfile/user.json';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     textTransform: 'uppercase',
+    //     color: '#010101',
+    //   }}
+    // >
+    //   React homework template
+    // </div>
+
+    <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
   );
 };
