d62040b56a5dadf994d79ab3aa8800725ea412e7
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-02-11 11:53:04 +0200

    11.02

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import { FriendItem, FrendStatus, FrendAvatar, FrendName } from './FriendListItem.styled.jsx';
 
-export const FriendListItem = ({ avatar, name, isOnline, id }) => {
+export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-   <FriendItem key={id}>
+   <FriendItem>
           <FrendStatus status={isOnline} ></FrendStatus>
       <FrendAvatar src={avatar} alt="User avatar" width="48" />
           <FrendName>{name}</FrendName>
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -17,21 +17,22 @@ export const Statistics = ({ title, stats }) => {
       label: "Unknown",
       percentage: 0
 };  
-
+  
+  const StatsMap =
+    stats.map(({ id, label, percentage }) => (
+          
+      <StatsItem key={id}>
+        <Label>{label}</Label>
+        <Percentage>{percentage}%</Percentage>
+            
+        </StatsItem>));
+  
   return (
-   
-<StatisticSec>
+   <StatisticSec>
 {title && <Title>Upload stats</Title>}
 
-      <Stats>
-        
-        {stats.map(({ id, label, percentage }) => (             
-          
-        <StatsItem key={id}>
-          <Label>{label}</Label>
-            <Percentage>{percentage}%</Percentage>
-            
-        </StatsItem>))}
+      <Stats>     
+        {StatsMap}
         </Stats>
     </StatisticSec>
       )
